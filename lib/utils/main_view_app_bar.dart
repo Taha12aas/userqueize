b59b502e8af4242a54fe20e995cc90ba963ea4d7
taha <tnttasata@gmail.com>
2024-12-11 21:11:31 +0300

    edit

--- lib/utils/main_view_app_bar.dart
+++ lib/utils/main_view_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:userqueize/Mobile/widgets/home_view/DropDownSearch/show_custom_drop_down_search.dart';
+
 PreferredSizeWidget mainViewAppBar(BuildContext context) {
   return PreferredSize(
     preferredSize: const Size.fromHeight(55),
@@ -12,36 +13,18 @@ PreferredSizeWidget mainViewAppBar(BuildContext context) {
         ),
       ),
       padding: const EdgeInsets.only(top: 10, left: 8, right: 8),
-      child: Builder(
-        builder: (BuildContext innerContext) => Row(
-          mainAxisAlignment:
-              MainAxisAlignment.spaceBetween, // Keep buttons on the edges
-          crossAxisAlignment:
-              CrossAxisAlignment.end, // Align items to the bottom
-          children: [
-            IconButton(
-              icon: const Icon(Icons.menu, color: Colors.white),
-              onPressed: () {
-                Scaffold.of(innerContext).openDrawer();
-              },
-            ),
-            const Align(
-              alignment: Alignment.bottomCenter, // Align the icon at the bottom
-              child: Icon(
-                Icons.school,
-                color: Colors.orange,
-                size: 50,
-              ),
-            ),
-            IconButton(
-              tooltip: 'search',
-              icon: const Icon(Icons.search, color: Colors.white),
-              onPressed: () {
-                showCustomDropDownSearch(context);
-              },
-            ),
-          ],
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end, // Keep buttons on the edges
+        crossAxisAlignment: CrossAxisAlignment.end, // Align items to the bottom
+        children: [
+          IconButton(
+            tooltip: 'search',
+            icon: const Icon(Icons.search, color: Colors.white),
+            onPressed: () {
+              showCustomDropDownSearch(context);
+            },
+          ),
+        ],
       ),
     ),
   );
