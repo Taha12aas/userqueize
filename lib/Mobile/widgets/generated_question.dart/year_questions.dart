055e3249798d01a66fb10568351261523269f265
salimo2002 <89202366+salimo2002@users.noreply.github.com>
2024-12-11 21:38:47 +0300

    remove valuenotifier and add setstate

--- lib/Mobile/widgets/generated_question.dart/year_questions.dart
+++ lib/Mobile/widgets/generated_question.dart/year_questions.dart
@@ -3,53 +3,55 @@ import 'package:userqueize/utils/constants.dart';
 import 'package:userqueize/utils/font_style.dart';
 import 'package:userqueize/utils/responsive_text.dart';
 
-class YearQuestions extends StatelessWidget {
+class YearQuestions extends StatefulWidget {
   const YearQuestions({
     super.key,
     required this.year,
   });
   final String year;
+
+  @override
+  State<YearQuestions> createState() => _YearQuestionsState();
+}
+
+class _YearQuestionsState extends State<YearQuestions> {
+  bool value = false;
   @override
   Widget build(BuildContext context) {
-    ValueNotifier<bool> valueNotifier = ValueNotifier(false);
-    return ValueListenableBuilder(
-      valueListenable: valueNotifier,
-      builder: (context, value, child) {
-        return Container(
-          height: MediaQuery.sizeOf(context).height * .06,
-          decoration: BoxDecoration(
-            color: kAshenColor,
-            borderRadius: BorderRadius.circular(16),
+    return Container(
+      height: MediaQuery.sizeOf(context).height * .06,
+      decoration: BoxDecoration(
+        color: kAshenColor,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Row(
+        children: [
+          const SizedBox(width: 10),
+          IconButton(
+            onPressed: () {
+              value = !value;
+              setState(() {});
+            },
+            icon: value
+                ? const Icon(
+                    Icons.file_download_done,
+                    color: Colors.white,
+                  )
+                : const Icon(
+                    Icons.sim_card_download_sharp,
+                    color: Colors.white,
+                  ),
           ),
-          child: Row(
-            children: [
-              const SizedBox(width: 10),
-              IconButton(
-                onPressed: () {
-                  valueNotifier.value = !valueNotifier.value;
-                },
-                icon: value
-                    ? const Icon(
-                        Icons.file_download_done,
-                        color: Colors.white,
-                      )
-                    : const Icon(
-                        Icons.sim_card_download_sharp,
-                        color: Colors.white,
-                      ),
-              ),
-              const Spacer(),
-              Text(
-                year,
-                style: FontStyleApp.snakBarLabel.copyWith(
-                  fontSize: getResponsiveText(context, 18),
-                ),
-              ),
-              const SizedBox(width: 20)
-            ],
+          const Spacer(),
+          Text(
+            widget.year,
+            style: FontStyleApp.snakBarLabel.copyWith(
+              fontSize: getResponsiveText(context, 18),
+            ),
           ),
-        );
-      },
+          const SizedBox(width: 20)
+        ],
+      ),
     );
   }
 }
