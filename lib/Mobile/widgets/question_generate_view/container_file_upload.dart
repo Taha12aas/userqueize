156e16f2cf7ae857cac2ef5cc652ff866689149e
taha <tnttasata@gmail.com>
2024-12-13 02:06:59 +0300

    edit ContainerFileUpload

--- lib/Mobile/widgets/question_generate_view/container_file_upload.dart
+++ lib/Mobile/widgets/question_generate_view/container_file_upload.dart
@@ -11,39 +11,44 @@ class ContainerFileUpload extends StatelessWidget {
     double screenWidth = MediaQuery.sizeOf(context).width;
     double screenHeight = MediaQuery.sizeOf(context).height;
 
-    return Container(
-      width: screenWidth * 0.9,
-      height: screenHeight * 0.27,
-      decoration: BoxDecoration(
-        color: Colors.orange,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(
-            Icons.cloud_upload,
-            size: 50,
-            color: Colors.black,
-          ),
-          const SizedBox(height: 10),
-          const Text('اسحب او اختر ملف', style: FontStyleApp.textStyleBold16),
-          const SizedBox(height: 20),
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.black,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+    return InkWell(
+      borderRadius: BorderRadius.circular(7),
+      onTap: () {},
+      child: Container(
+        width: screenWidth * 0.9,
+        height: screenHeight * 0.27,
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.cloud_upload,
+              size: 50,
+              color: Colors.black,
             ),
-            child: const Text(
-              'Browse files',
-              style: TextStyle(
-                color: Colors.orange, // لون النص
-                fontSize: 16,
+            const SizedBox(height: 10),
+            const Text('اسحب او اختر ملف', style: FontStyleApp.textStyleBold16),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.black,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+              child: const Text(
+                'Browse files',
+                style: TextStyle(
+                  color: Colors.orange, // لون النص
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
