b635e500a7cb176410bb9ed47ae856611e1cce90
salimo2002 <89202366+salimo2002@users.noreply.github.com>
2024-12-12 23:40:38 +0300

    create mainAppBar

--- lib/utils/main_app_bar.dart
+++ lib/utils/main_app_bar.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:userqueize/Mobile/views/teacher_profile_view.dart';
 import 'package:userqueize/Mobile/widgets/home_view/DropDownSearch/show_custom_drop_down_search.dart';
 import 'package:userqueize/utils/constants.dart';
 import 'package:userqueize/utils/font_style.dart';
 
-AppBar customAppBar(String title, BuildContext context) {
+AppBar mainAppBar(String title, BuildContext context) {
   return AppBar(
     centerTitle: true,
     shape: const RoundedRectangleBorder(
@@ -11,9 +12,24 @@ AppBar customAppBar(String title, BuildContext context) {
           bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16)),
     ),
     backgroundColor: kAshenColor,
-    title: Text(
-      title,
-      style: FontStyleApp.textStylewite15.copyWith(fontWeight: FontWeight.w800),
+    title: Row(
+      children: [
+        InkWell(
+          onTap: () {
+            Navigator.pushNamed(context, TeacherProfileView.id);
+          },
+          child: const CircleAvatar(
+            radius: 18.0,
+            backgroundImage: AssetImage('assets/images/TeachersTaha.jpg'),
+          ),
+        ),
+        SizedBox(width: MediaQuery.sizeOf(context).width*.26,),
+        Text(
+          title,
+          style: FontStyleApp.textStylewite15
+              .copyWith(fontWeight: FontWeight.w800),
+        ),
+      ],
     ),
     iconTheme: const IconThemeData(
       color: Colors.white,
