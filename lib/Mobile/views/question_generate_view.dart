022a7f4b271b3a75f6b9109d4113cf0440d6c502
tnttasata <tnttasata@gmail.com>
2024-12-12 21:48:32 +0300

    add QuestionGenerateView

---
+++ lib/Mobile/views/question_generate_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class QuestionGenerateView extends StatelessWidget {
+  const QuestionGenerateView({super.key});
+    static String id = 'QuestionGenerateView';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:userqueize/Mobile/views/create_subject_questions_view.dart';
 import 'package:userqueize/Mobile/views/generated_questions_view.dart';
 import 'package:userqueize/Mobile/views/home_view.dart';
 import 'package:userqueize/Mobile/views/log_in_view.dart';
+import 'package:userqueize/Mobile/views/question_generate_view.dart';
 import 'package:userqueize/Mobile/views/subjects_view.dart';
 import 'package:userqueize/Mobile/views/teacher_profile_view.dart';
 import 'package:userqueize/Mobile/views/teacher_subjects_view.dart';
@@ -31,6 +32,7 @@ class QuizApp extends StatelessWidget {
         CreateQuestionsView.id: (context) => const CreateQuestionsView(),
         CreateSubjectQuestionsView.id: (context) =>
             const CreateSubjectQuestionsView(),
+        QuestionGenerateView.id: (context) => const QuestionGenerateView(),
       },
       theme:
           ThemeData(scaffoldBackgroundColor: kBackGround, fontFamily: 'Exo2'),
