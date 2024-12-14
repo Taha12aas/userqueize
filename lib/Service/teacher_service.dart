9ac9c3a5274e6995a78be3016d99f9e0c5a0ab0e
taha <tnttasata@gmail.com>
2024-12-14 04:35:19 +0300

    edit TeacherService

--- lib/Service/teacher_service.dart
+++ lib/Service/teacher_service.dart
@@ -6,7 +6,6 @@ import 'package:userqueize/models/teacher.dart';
 class TeacherService {
   final SupabaseClient supabase = Supabase.instance.client;
 
-  // دالة لاسترجاع بيانات المعلم
   Future<Teacher?> fetchTeacher(int teacherId) async {
     try {
       final response = await supabase.from('teachers').select();
@@ -18,11 +17,9 @@ class TeacherService {
     return null;
   }
 
-  // دالة لإرسال بيانات معلم إلى Supabase
   Future<void> createTeacher(Teacher teacher) async {
     final response = await supabase.from('teachers').insert([teacher.toJson()]);
 
-    // التحقق من وجود خطأ في الاستجابة
     if (response.error != null) {
       throw Exception('فشل في إنشاء المعلم: ${response.error?.message}');
     }
