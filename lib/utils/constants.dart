5803a7ff217df00632edfb6229ad2110cb9b7a2e
taha <asdfghmnt@gmail.com>
2025-01-23 13:32:47 +0300

    kBackGround

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const kOrangeColor = Color(0xffF18536);
 const kAshenColor = Color(0xff373737);
-const kBackGround = Color(0xff191E2C);
+const kBackGround = Color(0xff0D1323);
 const kBlack = Colors.black;
 const kWhite = Colors.white;
 const kOrangeBlackColor = Color(0xff251E1A);
