c9c57ff6c7dec2673dc4536824e448ab2d976f24
taha <asdfghmnt@gmail.com>
2025-03-13 08:04:29 +0300

    edit ContainerTeacherSubjectsDisplay add list view

--- lib/Mobile/widgets/add_teacher_view/container_teache_subjects_display.dart
+++ lib/Mobile/widgets/add_teacher_view/container_teache_subjects_display.dart
@@ -37,66 +37,72 @@ class ContainerTeacherSubjectsDisplay extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: Table(
-                border: TableBorder.all(color: Colors.grey.withOpacity(0.2)),
-                columnWidths: const {
-                  0: FlexColumnWidth(2),
-                  1: FlexColumnWidth(1),
-                  2: FlexColumnWidth(1),
-                },
-                children: List.generate(classes.length, (index) {
-                  final color = index.isEven ? kWhite : const Color(0xffE4E4E4);
-                  return TableRow(
-                    decoration: BoxDecoration(
-                      color: color,
-                    ),
-                    children: [
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: GestureDetector(
-                            onTap: () {
-                              showModalBottomSheet(
-                                context: context,
-                                builder: (context) {
-                                  return const ContainerCourserUpload();
+              child: ListView(
+                children: [
+                  Table(
+                    border:
+                        TableBorder.all(color: Colors.grey.withOpacity(0.2)),
+                    columnWidths: const {
+                      0: FlexColumnWidth(2),
+                      1: FlexColumnWidth(1),
+                      2: FlexColumnWidth(1),
+                    },
+                    children: List.generate(classes.length, (index) {
+                      final color =
+                          index.isEven ? kWhite : const Color(0xffE4E4E4);
+                      return TableRow(
+                        decoration: BoxDecoration(
+                          color: color,
+                        ),
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: GestureDetector(
+                                onTap: () {
+                                  showModalBottomSheet(
+                                    context: context,
+                                    builder: (context) {
+                                      return const ContainerCourserUpload();
+                                    },
+                                  );
                                 },
-                              );
-                            },
-                            child: Text(
-                              'اضغط هنا لرفع دورة',
-                              style: FontStyleApp.orangeBold20.copyWith(
-                                fontSize: getResponsiveText(context, 18),
+                                child: Text(
+                                  'اضغط هنا لرفع دورة',
+                                  style: FontStyleApp.orangeBold20.copyWith(
+                                    fontSize: getResponsiveText(context, 18),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            subjects[index].toString(),
-                            style: FontStyleApp.blackBold16.copyWith(
-                              fontSize: getResponsiveText(context, 18),
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                subjects[index].toString(),
+                                style: FontStyleApp.blackBold16.copyWith(
+                                  fontSize: getResponsiveText(context, 18),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            classes[index].toString(),
-                            style: FontStyleApp.blackBold16.copyWith(
-                              fontSize: getResponsiveText(context, 18),
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                classes[index].toString(),
+                                style: FontStyleApp.blackBold16.copyWith(
+                                  fontSize: getResponsiveText(context, 18),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                    ],
-                  );
-                }),
+                        ],
+                      );
+                    }),
+                  ),
+                ],
               ),
             ),
           ],
