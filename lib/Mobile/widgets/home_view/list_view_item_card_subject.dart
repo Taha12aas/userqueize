b4a77b73332a9eb57838df047a1d2b218f151111
taha <asdfghmnt@gmail.com>
2025-03-19 23:59:15 +0300

    edit

--- lib/Mobile/widgets/home_view/list_view_item_card_subject.dart
+++ lib/Mobile/widgets/home_view/list_view_item_card_subject.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:userqueize/Mobile/views/reading_generated_questions.dart';
 import 'package:userqueize/Mobile/widgets/teachers_view_and_subjects_view/card_subjects.dart';
 import 'package:userqueize/cubits/cubitSubject/cubit_subject.dart';
 import 'package:userqueize/cubits/cubitSubject/cubit_subject_status.dart';
 import 'package:userqueize/utils/font_style.dart';
+
+import 'package:pdf/widgets.dart' as pw;
 
 class ListViewItemCardSubject extends StatelessWidget {
   const ListViewItemCardSubject({
@@ -23,6 +29,7 @@ class ListViewItemCardSubject extends StatelessWidget {
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 10),
                 child: CardSubjects(
+
                   courseDate: CubitSubject.subjectsCount[index].coursesDate,
                   seasonSubject:
                       CubitSubject.subjectsCount[index].seasonSubject,
@@ -51,4 +58,89 @@ class ListViewItemCardSubject extends StatelessWidget {
       },
     );
   }
+  
+  Future<void> generatePdfFile(List subjectInfo) async {
+    // تحميل الخط العربي
+    
+    final arabicFont = pw.Font.ttf(
+      await rootBundle.load('assets/fonts/Amiri-Bold.ttf'),
+    );
+
+    // إنشاء مستند PDF
+    final pdf = pw.Document();
+
+    // البحث عن الدورات المطابقة
+    List<dynamic> course = [];
+    for (var i = 0; i < CubitSubject.subjectsCount.length; i++) {
+      if (subjectInfo[0] == CubitSubject.subjectsCount[i].nameSubject &&
+          subjectInfo[1] == CubitSubject.subjectsCount[i].classSabject &&
+          subjectInfo[2] == CubitSubject.subjectsCount[i].coursesDate &&
+          subjectInfo[3] == CubitSubject.subjectsCount[i].seasonSubject &&
+          subjectInfo[4] == CubitSubject.subjectsCount[i].generateTime) {
+        course.addAll(CubitSubject.subjectsCount[i].courses);
+      }
+    }
+
+    // إضافة محتوى إلى PDF
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Column(
+            crossAxisAlignment: pw.CrossAxisAlignment.start,
+            children: [
+              // عنوان الصفحة
+              pw.Text(
+                'تفاصيل الدورات',
+                style: pw.TextStyle(
+                  fontSize: 24,
+                  fontWeight: pw.FontWeight.bold,
+                  font: arabicFont,
+                ),
+                textDirection: pw.TextDirection.rtl,
+                textAlign: pw.TextAlign.right,
+              ),
+              pw.SizedBox(height: 20),
+
+              // تكرار المحتوى من الـ course
+              ...course.map((courseItem) {
+                return pw.Column(
+                  crossAxisAlignment: pw.CrossAxisAlignment.start,
+                  children: [
+                    pw.Text(
+                      'اسم الدورة: ${courseItem['name']}', // افترض أن courseItem يحتوي على حقل 'name'
+                      style: pw.TextStyle(
+                        fontSize: 18,
+                        font: arabicFont,
+                      ),
+                      textDirection: pw.TextDirection.rtl,
+                      textAlign: pw.TextAlign.right,
+                    ),
+                    pw.Text(
+                      'الوصف: ${courseItem['description']}', // افترض أن courseItem يحتوي على حقل 'description'
+                      style: pw.TextStyle(
+                        fontSize: 16,
+                        font: arabicFont,
+                      ),
+                      textDirection: pw.TextDirection.rtl,
+                      textAlign: pw.TextAlign.right,
+                    ),
+                    pw.SizedBox(height: 10),
+                  ],
+                );
+              }).toList(),
+            ],
+          );
+        },
+      ),
+    );
+
+    // حفظ الملف
+    final outputDir = await getApplicationDocumentsDirectory();
+    final filePath = '${outputDir.path}/courses_details.pdf';
+    final file = File(filePath);
+    await file.writeAsBytes(await pdf.save());
+
+    print('File saved to $filePath');
+  }
+
 }
--- lib/Mobile/widgets/teachers_view_and_subjects_view/card_subjects.dart
+++ lib/Mobile/widgets/teachers_view_and_subjects_view/card_subjects.dart
@@ -1,13 +1,8 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bounceable/flutter_bounceable.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:userqueize/Mobile/widgets/teachers_view_and_subjects_view/row_home_view.dart';
-import 'package:userqueize/cubits/cubitSubject/cubit_subject.dart';
 import 'package:userqueize/cubits/cubitTeacher/cubit_teacher.dart';
 import 'package:userqueize/utils/constants.dart';
-import 'package:pdf/widgets.dart' as pw;
 
 class CardSubjects extends StatelessWidget {
   const CardSubjects({
@@ -16,7 +11,7 @@ class CardSubjects extends StatelessWidget {
     required this.subject,
     required this.classTeacher,
     required this.courseDate,
-    required this.seasonSubject,
+    required this.seasonSubject, this.onPressed,
   });
 
   final void Function() onTap;
@@ -24,92 +19,10 @@ class CardSubjects extends StatelessWidget {
   final String classTeacher;
   final String courseDate;
   final String seasonSubject;
-  Future<void> generatePdfFile(List subjectInfo) async {
-    // تحميل الخط العربي
-    final arabicFont = pw.Font.ttf(
-      await rootBundle.load('assets/fonts/Amiri-Bold.ttf'),
-    );
-
-    // إنشاء مستند PDF
-    final pdf = pw.Document();
-
-    // البحث عن الدورات المطابقة
-    List<dynamic> course = [];
-    for (var i = 0; i < CubitSubject.subjectsCount.length; i++) {
-      if (subjectInfo[0] == CubitSubject.subjectsCount[i].nameSubject &&
-          subjectInfo[1] == CubitSubject.subjectsCount[i].classSabject &&
-          subjectInfo[2] == CubitSubject.subjectsCount[i].coursesDate &&
-          subjectInfo[3] == CubitSubject.subjectsCount[i].seasonSubject &&
-          subjectInfo[4] == CubitSubject.subjectsCount[i].generateTime) {
-        course.addAll(CubitSubject.subjectsCount[i].courses);
-      }
-    }
-
-    // إضافة محتوى إلى PDF
-    pdf.addPage(
-      pw.Page(
-        build: (pw.Context context) {
-          return pw.Column(
-            crossAxisAlignment: pw.CrossAxisAlignment.start,
-            children: [
-              // عنوان الصفحة
-              pw.Text(
-                'تفاصيل الدورات',
-                style: pw.TextStyle(
-                  fontSize: 24,
-                  fontWeight: pw.FontWeight.bold,
-                  font: arabicFont,
-                ),
-                textDirection: pw.TextDirection.rtl,
-                textAlign: pw.TextAlign.right,
-              ),
-              pw.SizedBox(height: 20),
-
-              // تكرار المحتوى من الـ course
-              ...course.map((courseItem) {
-                return pw.Column(
-                  crossAxisAlignment: pw.CrossAxisAlignment.start,
-                  children: [
-                    pw.Text(
-                      'اسم الدورة: ${courseItem['name']}', // افترض أن courseItem يحتوي على حقل 'name'
-                      style: pw.TextStyle(
-                        fontSize: 18,
-                        font: arabicFont,
-                      ),
-                      textDirection: pw.TextDirection.rtl,
-                      textAlign: pw.TextAlign.right,
-                    ),
-                    pw.Text(
-                      'الوصف: ${courseItem['description']}', // افترض أن courseItem يحتوي على حقل 'description'
-                      style: pw.TextStyle(
-                        fontSize: 16,
-                        font: arabicFont,
-                      ),
-                      textDirection: pw.TextDirection.rtl,
-                      textAlign: pw.TextAlign.right,
-                    ),
-                    pw.SizedBox(height: 10),
-                  ],
-                );
-              }).toList(),
-            ],
-          );
-        },
-      ),
-    );
-
-    // حفظ الملف
-    final outputDir = await getApplicationDocumentsDirectory();
-    final filePath = '${outputDir.path}/courses_details.pdf';
-    final file = File(filePath);
-    await file.writeAsBytes(await pdf.save());
-
-    print('File saved to $filePath');
-  }
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    List subjectInfo = ModalRoute.of(context)!.settings.arguments as List;
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
     return Bounceable(
@@ -141,9 +54,7 @@ class CardSubjects extends StatelessWidget {
                 ? IconButton(
                     color: kOrange,
                     iconSize: 30,
-                    onPressed: () {
-                      generatePdfFile(subjectInfo);
-                    }, // استدعاء الدالة هنا
+                    onPressed: onPressed,
                     icon: const Icon(
                       Icons.download,
                     ),
