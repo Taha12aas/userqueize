979c4b57913d65272880436305f094643b43ea90
tnttasata <tnttasata@gmail.com>
2025-03-20 00:54:41 +0300

    edit

--- lib/Mobile/widgets/home_view/list_view_item_card_subject.dart
+++ lib/Mobile/widgets/home_view/list_view_item_card_subject.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,7 +8,6 @@ import 'package:userqueize/Mobile/widgets/teachers_view_and_subjects_view/card_s
 import 'package:userqueize/cubits/cubitSubject/cubit_subject.dart';
 import 'package:userqueize/cubits/cubitSubject/cubit_subject_status.dart';
 import 'package:userqueize/utils/font_style.dart';
-
 import 'package:pdf/widgets.dart' as pw;
 
 class ListViewItemCardSubject extends StatelessWidget {
@@ -29,7 +27,69 @@ class ListViewItemCardSubject extends StatelessWidget {
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 10),
                 child: CardSubjects(
-
+                  onPressed: () async {
+                    List courses = CubitSubject.subjectsCount[index].courses;
+                    final arabicFont = pw.Font.ttf(
+                      await rootBundle.load('assets/fonts/Amiri-Bold.ttf'),
+                    );
+                    final pdf = pw.Document();
+                    pdf.addPage(
+                      pw.Page(
+                        build: (pw.Context context) {
+                          int questionCounter = 0;
+                          return pw.Column(
+                            crossAxisAlignment: pw.CrossAxisAlignment.start,
+                            children: [
+                              pw.Text(
+                                'اسم الدورة : ${CubitSubject.subjectsCount[index].nameSubject}',
+                                style: pw.TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: pw.FontWeight.bold,
+                                  font: arabicFont,
+                                ),
+                                textDirection: pw.TextDirection.rtl,
+                                textAlign: pw.TextAlign.right,
+                              ),
+                              pw.SizedBox(height: 20),
+                              ...courses.map((courseItem) {
+                                questionCounter++;
+                                return pw.Column(
+                                  crossAxisAlignment:
+                                      pw.CrossAxisAlignment.start,
+                                  children: [
+                                    pw.Text(
+                                      '$questionCounter ${courseItem['question']}', // افترض أن courseItem يحتوي على حقل 'name'
+                                      style: pw.TextStyle(
+                                        fontSize: 18,
+                                        font: arabicFont,
+                                      ),
+                                      textDirection: pw.TextDirection.rtl,
+                                      textAlign: pw.TextAlign.right,
+                                    ),
+                                    pw.Text(
+                                      'الاجابات:\n ${courseItem['answers']}', // افترض أن courseItem يحتوي على حقل 'description'
+                                      style: pw.TextStyle(
+                                        fontSize: 16,
+                                        font: arabicFont,
+                                      ),
+                                      textDirection: pw.TextDirection.rtl,
+                                      textAlign: pw.TextAlign.right,
+                                    ),
+                                    pw.SizedBox(height: 10),
+                                  ],
+                                );
+                              }),
+                            ],
+                          );
+                        },
+                      ),
+                    );
+                    final outputDir = await getApplicationDocumentsDirectory();
+                    final filePath = '${outputDir.path}/courses_details.pdf';
+                    final file = File(filePath);
+                    await file.writeAsBytes(await pdf.save());
+                    print('File saved to $filePath');
+                  },
                   courseDate: CubitSubject.subjectsCount[index].coursesDate,
                   seasonSubject:
                       CubitSubject.subjectsCount[index].seasonSubject,
@@ -58,89 +118,4 @@ class ListViewItemCardSubject extends StatelessWidget {
       },
     );
   }
-  
-  Future<void> generatePdfFile(List subjectInfo) async {
-    // تحميل الخط العربي
-    
-    final arabicFont = pw.Font.ttf(
-      await rootBundle.load('assets/fonts/Amiri-Bold.ttf'),
-    );
-
-    // إنشاء مستند PDF
-    final pdf = pw.Document();
-
-    // البحث عن الدورات المطابقة
-    List<dynamic> course = [];
-    for (var i = 0; i < CubitSubject.subjectsCount.length; i++) {
-      if (subjectInfo[0] == CubitSubject.subjectsCount[i].nameSubject &&
-          subjectInfo[1] == CubitSubject.subjectsCount[i].classSabject &&
-          subjectInfo[2] == CubitSubject.subjectsCount[i].coursesDate &&
-          subjectInfo[3] == CubitSubject.subjectsCount[i].seasonSubject &&
-          subjectInfo[4] == CubitSubject.subjectsCount[i].generateTime) {
-        course.addAll(CubitSubject.subjectsCount[i].courses);
-      }
-    }
-
-    // إضافة محتوى إلى PDF
-    pdf.addPage(
-      pw.Page(
-        build: (pw.Context context) {
-          return pw.Column(
-            crossAxisAlignment: pw.CrossAxisAlignment.start,
-            children: [
-              // عنوان الصفحة
-              pw.Text(
-                'تفاصيل الدورات',
-                style: pw.TextStyle(
-                  fontSize: 24,
-                  fontWeight: pw.FontWeight.bold,
-                  font: arabicFont,
-                ),
-                textDirection: pw.TextDirection.rtl,
-                textAlign: pw.TextAlign.right,
-              ),
-              pw.SizedBox(height: 20),
-
-              // تكرار المحتوى من الـ course
-              ...course.map((courseItem) {
-                return pw.Column(
-                  crossAxisAlignment: pw.CrossAxisAlignment.start,
-                  children: [
-                    pw.Text(
-                      'اسم الدورة: ${courseItem['name']}', // افترض أن courseItem يحتوي على حقل 'name'
-                      style: pw.TextStyle(
-                        fontSize: 18,
-                        font: arabicFont,
-                      ),
-                      textDirection: pw.TextDirection.rtl,
-                      textAlign: pw.TextAlign.right,
-                    ),
-                    pw.Text(
-                      'الوصف: ${courseItem['description']}', // افترض أن courseItem يحتوي على حقل 'description'
-                      style: pw.TextStyle(
-                        fontSize: 16,
-                        font: arabicFont,
-                      ),
-                      textDirection: pw.TextDirection.rtl,
-                      textAlign: pw.TextAlign.right,
-                    ),
-                    pw.SizedBox(height: 10),
-                  ],
-                );
-              }).toList(),
-            ],
-          );
-        },
-      ),
-    );
-
-    // حفظ الملف
-    final outputDir = await getApplicationDocumentsDirectory();
-    final filePath = '${outputDir.path}/courses_details.pdf';
-    final file = File(filePath);
-    await file.writeAsBytes(await pdf.save());
-
-    print('File saved to $filePath');
-  }
-
 }
